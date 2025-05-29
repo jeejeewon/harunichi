e8c4c9ba5789d6d1640fbd54acdb0d2d5dd4142b
jeewon <tnstlasla@gmail.com>
2025-05-29 14:50:07 +0900

    250229 upload

--- src/main/java/com/harunichi/board/dao/BoardDao.java
+++ src/main/java/com/harunichi/board/dao/BoardDao.java
@@ -1,10 +1,23 @@
 package com.harunichi.board.dao;
 
+
 import java.util.List;
 import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 
 public interface BoardDao {
 	
+//	@Autowired
+//    private SqlSession sqlSession;
+// 
+//    public void setSqlSession(SqlSession sqlSession){
+//        this.sqlSession = sqlSession;
+//    }
+//    
+	
+
 	
 }
---
+++ src/main/java/com/harunichi/board/dao/BoardDaoImpl.java
@@ -0,0 +1,8 @@
+package com.harunichi.board.dao;
+
+import org.springframework.stereotype.Repository;
+
+//@Repository("boardDao")
+//public class BoardDaoImpl implements BoardDao {
+//
+//}
