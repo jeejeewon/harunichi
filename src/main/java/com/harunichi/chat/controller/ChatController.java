5fd0dc390ebc4654e3c3b3020896a7e1454560a0
hyemi <gpal7355@naver.com>
2025-06-02 18:55:25 +0900

    추천 친구 기능 구현 중...

--- src/main/java/com/harunichi/chat/controller/ChatController.java
+++ src/main/java/com/harunichi/chat/controller/ChatController.java
@@ -1,35 +1,53 @@
 package com.harunichi.chat.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.harunichi.chat.service.ChatService;
+import com.harunichi.member.vo.MemberVo;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 @RequestMapping("/chat")
 public class ChatController {
-
+	
+	@Autowired
+	ChatService chatService;
+	
 	@RequestMapping("/login")
-	public String loginTest(HttpServletRequest request, HttpServletResponse response) throws Exception{
-		
-		System.out.println("chatController의 loginTest 메소드 실행 -------------");
-		
-		return "/chat/loginTest";
-		
+	public String loginTest(HttpServletRequest request, HttpServletResponse response) throws Exception{		
+		System.out.println("chatController의 loginTest 메소드 실행 -------------");		
+		return "/chat/loginTest";		
 	}
+	
 	
 	@RequestMapping("/main")
 	public String chatMain(HttpServletRequest request, 
-						   HttpServletResponse response) throws Exception{
+						   HttpServletResponse response, HttpSession session) throws Exception{
 		
 		System.out.println("chatController의 chatMain 메소드 실행 -------------");
+	
+		//로그인 했다고 가정하에 작성 (추후 삭제 필요)
+		String id = "hong";
+		session.setAttribute("id", id);
 		
-		//------ 인터셉터에서 로그인 인증 후 왔다고 가정하에 코드 작성
+		//DB에서 친구추천 리스트 조회
+		List<MemberVo> members = chatService.selectMembers(id);
+			//1. 사용자의 myLike(관심사)를 토대로 
+				//1-1. 관심사가 없거나 관심사에 맞는 사람이 없을 경우 그냥 조회
+			//2. 자신 빼고 조회
 		
-		//session에 사용자 정보가 저장 되어 있을거임!
-		//그걸 들고 chatMain으로 이동!		
+		
+		
 		
 		return "/chatMain";
 		
--- src/main/java/com/harunichi/chat/service/ChatService.java
+++ src/main/java/com/harunichi/chat/service/ChatService.java
@@ -1,10 +1,13 @@
 package com.harunichi.chat.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.harunichi.chat.dao.ChatDao;
 import com.harunichi.chat.vo.ChatVo;
+import com.harunichi.member.vo.MemberVo;
 
 @Service
 public class ChatService {
@@ -12,13 +15,17 @@ public class ChatService {
 	@Autowired
 	private ChatDao chatDao;
 	
+	
 	//채팅 메세지 DB에 저장
-	public void saveMessage(ChatVo chatMsg) {
-		
-		System.out.println("ChatService의 saveMessage메소드 호출 ===================");
-		
+	public void saveMessage(ChatVo chatMsg) {		
+		System.out.println("ChatService의 saveMessage메소드 호출 ===================");		
 		//DAO로 DB작업 요청시키기
 		chatDao.saveMessage(chatMsg);
+	}
+
+	//DB에서 친구 추천 리스트 조회
+	public List<MemberVo> selectMembers(String id) {
+		return chatDao.selectMembers(id);
 	}
 	
 	
