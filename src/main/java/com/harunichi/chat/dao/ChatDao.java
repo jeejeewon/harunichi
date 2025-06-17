12b78927f5339c69434628867214c6f36e061096
hyeming-way <gpal7355@naver.com>
2025-06-17 23:15:27 +0900

    Controller, Service, Dao 인터페이스화

--- src/main/java/com/harunichi/chat/dao/ChatDao.java
+++ src/main/java/com/harunichi/chat/dao/ChatDao.java
@@ -1,159 +1,34 @@
 package com.harunichi.chat.dao;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
 import com.harunichi.chat.vo.ChatRoomVo;
 import com.harunichi.chat.vo.ChatVo;
 import com.harunichi.member.vo.MemberVo;
 
-import lombok.extern.slf4j.Slf4j;
+public interface ChatDao {
 
-@Slf4j
-@Repository
-public class ChatDao {
+	void saveMessage(ChatVo chatMsg);
 
-	@Autowired
-	private SqlSession sqlSession;		
-	@Autowired
-	private ChatRoomVo chatRoomVo;
-	
-	private static final String NAMESPACE = "mapper.chat.";
+	List<MemberVo> selectMembers(String id);
 
-		
-	//채팅 저장
-	public void saveMessage(ChatVo chatMsg) {		
-		System.out.println("===ChatDao의 saveMessage 메소드 실행");	
-		sqlSession.insert(NAMESPACE + "saveMessage", chatMsg);			
-	}
+	List<MemberVo> selectRandomMembers();
 
-	
-	
-	//친구 추천 리스트 조회
-	public List<MemberVo> selectMembers(String id) {
-		System.out.println("===ChatDao의 selectMembers 메소드 실행");		
-		
-		//사용자의 관심사 조회하여 변수에 저장
-		String myLike = sqlSession.selectOne(NAMESPACE + "selectMyLike", id);		
-		System.out.println("myLike : " + myLike);
-		System.out.println("id : " + id);
-		
-		//조회한 관심사를 ","로 구분하여 자른 후 List에 저장
-		List<String> likeList = Arrays.stream(myLike.split(","))
-								      .map(String::trim) 
-								      .collect(Collectors.toList());
-		
-		//map에 관심사와 아이디를 저장하여 매퍼파일 매개변수로 전달
-		Map<String, Object> MyLikeMap = new HashMap<String, Object>();
-		MyLikeMap.put("likeList", likeList);
-		MyLikeMap.put("id", id);
-		
-		//관심사와 연령대가 비슷한 사람 10명 조회하여 List에 저장
-		List<MemberVo> resultList = sqlSession.selectList(NAMESPACE + "selectMembers", MyLikeMap);
-		
-		//List에 10명이 저장되지않았을 경우 (랜덤으로 추천하기 위함)
-		if (resultList.size() < 10) {
-		    int shortage = 10 - resultList.size();	//부족한 멤버수 계산하여 저장
-		    
-		    //조회된 멤버들 중복 조회 방지를 위해 조회된 멤버들 id를 List에 저장
-		    List<String> alreadySelectedIds = resultList.stream()
-									          .map(MemberVo::getId)
-									          .collect(Collectors.toList());
-		    
-		    //사용자 아이디, 조회된 멤버 아이디, 부족한 멤버수를 Map에 저장하여 매퍼파일 매개변수로 전달
-		    Map<String, Object> randomMap = new HashMap<>();
-		    randomMap.put("id", id);
-		    randomMap.put("alreadySelectedIds", alreadySelectedIds);
-		    randomMap.put("shortage", shortage);
+	String selectRoomId(String senderId, String receiverId, String chatType);
 
-		    List<MemberVo> randomMembers = sqlSession.selectList(NAMESPACE + "selectRandomMember", randomMap);
-		    resultList.addAll(randomMembers);
-		}
-		return resultList;		
-	}
-	
-	
-	//친구 추천 리스트 조회(비로그인)
-	public List<MemberVo> selectRandomMembers() {
-		return sqlSession.selectList(NAMESPACE + "selectRandomMembers");
-	}
+	void insertRoomId(Map<String, Object> roomMap);
 
-		
-	//DB에서 채팅방 ID 조회
-	public String selectRoomId(String senderId, String receiverId, String chatType) {		
-		System.out.println("===ChatDao의 selectRoomId 메소드 실행");	
-		
-		Map<String, String> idMap = new HashMap<String, String>();
-		idMap.put("senderId", senderId);
-		idMap.put("receiverId", receiverId);
-		idMap.put("chatType", chatType);
-			
-		return sqlSession.selectOne(NAMESPACE + "selectRoomId", idMap);
-	}	
+	List<ChatVo> selectChatHistory(String roomId);
 
-	//DB의 chatRoom테이블에 roomId 저장
-	public void insertRoomId(Map<String, Object> roomMap) {
-		System.out.println("===ChatDao의 insertRoomId 메소드 실행");
-		sqlSession.insert(NAMESPACE + "insertRoomId", roomMap);				
-	}
-	
+	int selectUserCount(String roomId);
 
-	//DB에서 채팅 내역 조회
-	public List<ChatVo> selectChatHistory(String roomId) {
-		System.out.println("===ChatDao의 selectChatHistory 메소드 실행");
-		return sqlSession.selectList(NAMESPACE + "selectChatHistory", roomId);
-	}
+	String selectTitle(String roomId);
 
+	MemberVo selectNick(String receiverId);
 
-	//채팅방에 참여하고 있는 유저 조회
-	public int selectUserCount(String roomId) {
-		System.out.println("===ChatDao의 selectUserCount 메소드 실행");
-		return sqlSession.selectOne(NAMESPACE + "selectUserCount", roomId);
-	}
+	List<ChatRoomVo> selectOpenChat();
 
+	List<ChatVo> selectMyChat(String id);
 
-	//채팅방 타이틀 조회(단체)
-	public String selectTitle(String roomId) {		
-		System.out.println("===ChatDao의 selectTitle 메소드 실행");
-		return sqlSession.selectOne(NAMESPACE + "selectTitle", roomId);
-	}
-
-
-	//채팅방 타이틀 조회(개인)
-	public MemberVo selectNick(String receiverId) {
-		System.out.println("===ChatDao의 selectNick 메소드 실행");
-		return sqlSession.selectOne(NAMESPACE + "selectNick", receiverId);
-	}
-
-	
-	//오픈 채팅방 조회
-	public List<ChatRoomVo> selectOpenChat() {
-		System.out.println("===ChatDao의 selectOpenChat 메소드 실행");
-		return sqlSession.selectList(NAMESPACE + "selectOpenChat");
-	}
-
-
-	//내가 참여중인 채팅방 조회
-	public List<ChatVo> selectMyChat(String id) {
-		System.out.println("===ChatDao의 selectMyChat 메소드 실행");
-		return sqlSession.selectList(NAMESPACE + "selectMyChat", id);
-	}
-
-
-
-
-
-
-
-
-
-	
-	
 }
--- src/main/java/com/harunichi/chat/service/ChatService.java
+++ src/main/java/com/harunichi/chat/service/ChatService.java
@@ -1,200 +1,31 @@
 package com.harunichi.chat.service;
 
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Random;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import com.harunichi.chat.dao.ChatDao;
 import com.harunichi.chat.vo.ChatRoomVo;
 import com.harunichi.chat.vo.ChatVo;
 import com.harunichi.member.vo.MemberVo;
 
-@Service
-public class ChatService {
+public interface ChatService {
 
-	@Autowired
-	private ChatDao chatDao;
-	
-	//채팅 메세지 DB에 저장
-	public void saveMessage(ChatVo chatMsg) {		
-		System.out.println("---ChatService의 saveMessage메소드 호출");		
-		//DAO로 DB작업 요청시키기
-		chatDao.saveMessage(chatMsg);
-	}
+	void saveMessage(ChatVo chatMsg);
 
-	
-	//DB에서 친구 추천 리스트 조회
-	public List<MemberVo> selectMembers(String id) {
-		System.out.println("---ChatService의 selectMembers메소드 호출");			
-		//비로그인
-		if(id == null) {
-			return chatDao.selectRandomMembers();
-		//로그인
-		}else {
-			return chatDao.selectMembers(id);
-		}	
-	}
-	
-	
-	//채팅방 ID 조회
-	public String selectRoomId(String senderId, String receiverId,  String chatType) {
-		System.out.println("---ChatService의 selectRoomId메소드 호출");		
-		
-		String roomId = chatDao.selectRoomId(senderId, receiverId, chatType);		
-				
-		ChatRoomVo vo = new ChatRoomVo();
-		vo.setUserId(senderId);
-		vo.setReceiverId(receiverId);
-		vo.setChatType(chatType);
-		
-		MemberVo memberVo = chatDao.selectNick(receiverId);
-		vo.setTitle(memberVo.getNick());
-					
-		System.out.println("roomId : " + roomId);
-		
-		//DB에 조회된 채팅방ID가 없다면?
-		if(roomId == null) {		
-			roomId = insertRoomId(vo);
-		}	
-		
-		//DB에서 조회된 채팅방ID 반환
-		return roomId;
-	}
-	
-	
-	public String insertRoomId(ChatRoomVo vo) {
-		
-		System.out.println("---ChatService의 insertRoomId메소드 호출");		
-		
-		Map<String, Object> roomMap = new HashMap<String, Object>();
-	
-		//새로운 채팅방 아이디 생성
-		//현재 날짜
-		String now = new SimpleDateFormat("yyyyMMdd").format(new Date());		
-		//랜덤 숫자 (0 ~ 999999)
-		int randomInt = new Random().nextInt(1_000_000);			
-		//6자리로 포맷 (앞에 0 채움)
-		String random = String.format("%06d", randomInt);			
-		//새로운 채팅방 ID 생성 후 반환
-		String newRoomId = now + "_" + random;			
-		System.out.println("newRoomId : " + newRoomId);
-		
-		roomMap.put("roomId", newRoomId);
-		
-		//단체 채팅일 경우
-		if(vo.getChatType().equals("group")) {			
-			roomMap.put("userId", vo.getUserId());
-			roomMap.put("title", vo.getTitle());
-		
-		//개인 채팅일 경우
-		}else {
-			List<String> userList = new ArrayList<String>();		
-			userList.add(vo.getReceiverId());
-			userList.add(vo.getUserId());
-			roomMap.put("userList", userList);
-			roomMap.put("title", vo.getTitle());
-		}
-		
-		roomMap.put("chatType", vo.getChatType());
-		roomMap.put("persons", vo.getPersons());
-						
-		//DB의 chatRoom테이블에 채팅방 정보 저장
-		chatDao.insertRoomId(roomMap);
-		
-		return newRoomId;		
-	}
+	List<MemberVo> selectMembers(String id);
 
-	
-	//과거 채팅 내역 불러오기
-	public List<ChatVo> selectChatHistory(String roomId) {
-		System.out.println("---ChatService의 selectChatHistory메소드 호출");	
-		return chatDao.selectChatHistory(roomId);
-	}
+	String selectRoomId(String senderId, String receiverId, String chatType);
 
-	
-	//채팅방 참여 인원 확인
-	public int selectUserCount(String roomId) {		
-		System.out.println("---ChatService의 selectUserCount메소드 호출");		
-		return chatDao.selectUserCount(roomId);
-	}
-	
+	String insertRoomId(ChatRoomVo vo);
 
-	//채팅방 타이틀 확인(단체채팅)
-	public String selectTitle(String roomId) {
-		System.out.println("---ChatService의 selectTitle메소드 호출");
-		System.out.println("roomId : " + roomId);
-		return chatDao.selectTitle(roomId);
-	}
-	
+	List<ChatVo> selectChatHistory(String roomId);
 
-	//채팅방 타이틀 확인(개인채팅)
-	public MemberVo selectNick(String receiverId) {
-		System.out.println("---ChatService의 selectNick메소드 호출");
-		return chatDao.selectNick(receiverId);
-	}
+	int selectUserCount(String roomId);
 
-	
-	//오픈 채팅방 리스트 조회
-	public List<ChatRoomVo> selectOpenChat() {	
-		System.out.println("---ChatService의 selectOpenChat메소드 호출");
-		List<ChatRoomVo> openChatList = chatDao.selectOpenChat();
-		
-		//채팅방 참여 인원 확인
-		for(ChatRoomVo vo : openChatList) {
-			int count = selectUserCount(vo.getRoomId());
-			vo.setUserCount(count);
-		}
-		
-		return openChatList;
-	}
+	String selectTitle(String roomId);
 
-	
-	//내 채팅 목록 조회
-	public List<ChatVo> selectMyChat(String id) {
-		System.out.println("---ChatService의 selectMyChat메소드 호출");
-		
-		//최신 채팅 정보 조회
-		List<ChatVo> myChatList = chatDao.selectMyChat(id);
-					
-		
-		//채팅 목록에 나타낼 최신 메세지 시간 나타내기 위한 반복문
-		for(ChatVo vo : myChatList) {
-			Timestamp sentTime = vo.getSentTime();
-			
-			//Timestamp -> LocalDateTime 데이터타입 변환
-			LocalDateTime sentDateTime = sentTime.toLocalDateTime();
-			
-			LocalDate today = LocalDate.now();
-			LocalDate sentDate = sentDateTime.toLocalDate();
-			
-			//날짜와 시간 포맷팅
-			DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("a hh:mm", Locale.KOREAN); 
-			DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-			
-			//조건문에서 포맷팅한 날짜와 시간 담을 변수
-			String displayTime;
-			
-			if (sentDate.equals(today)) {
-			    displayTime = sentDateTime.format(timeFormatter);
-			} else {
-			    displayTime = sentDateTime.format(dateFormatter);
-			}
-			vo.setDisplayTime(displayTime);	
-		}
-		
-		return myChatList;
-		
-	}
+	MemberVo selectNick(String receiverId);
+
+	List<ChatRoomVo> selectOpenChat();
+
+	List<ChatVo> selectMyChat(String id);
+
 }
-
