cbed7e04a7cb9b98ff5191fff92cfafa284847a0
hyunju <mira2539@gmail.com>
2025-06-24 14:32:55 +0900

    0624/수정,추가,추가

---
+++ src/main/java/com/harunichi/main/controller/MainController.java
@@ -0,0 +1,69 @@
+package com.harunichi.main.controller;
+
+import java.util.Locale;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+	
+	//메인 페이지를 보여주는 메서드
+	//http://localhost:8090/harunichi 요청시 메인페이지, 또는
+	//http://localhost:8090/harunichi/main 요청시 메인페이지
+	@RequestMapping(value = {"/", "/main"}, method = RequestMethod.GET)
+	public String showMainPage(Locale locale, Model model, HttpServletRequest request) {
+		
+		logger.info("메인페이지입니다.", locale);
+		
+		// 인터셉터가 넣어둔 viewName 가져오기
+		String viewName = (String) request.getAttribute("viewName");
+		System.out.println("컨트롤러에서 가져온 viewName: " + viewName);
+		
+		// 세션에 국가 정보 확인 및 설정
+	    HttpSession session = request.getSession();
+	    String selectedCountry = (String) session.getAttribute("selectedCountry");
+	    // 세션에 국가 정보가 없으면 기본값 'kr'로 설정
+	    if (selectedCountry == null || selectedCountry.isEmpty()) {
+	        session.setAttribute("selectedCountry", "kr");
+	        logger.info("세션에 국가 정보가 없어 기본값 'kr' 저장.");
+	        selectedCountry = "kr";
+	    } else {
+	        logger.info("세션에 이미 저장된 국가 정보 확인: " + selectedCountry);
+	    }
+
+	    // 세션에 저장된 국가 정보를 Model에 담아서 JSP로 전달
+	    model.addAttribute("selectedCountry", selectedCountry);
+		
+		logger.info("MainController - showMainPage() 메소드 종료. Returning view name: /main");
+		return "/main"; 
+	}
+	
+	
+	//세션에 국가를 저장하는 메서드
+	@RequestMapping(value = "/main/selectCountry", method = RequestMethod.POST)
+	@ResponseBody // 이 메서드는 뷰 이름을 반환하는 대신 응답 본문에 직접 내용을 씁니다. (AJAX 통신 시 주로 사용)
+	public String selectCountry(@RequestParam("nationality") String nationality, HttpServletRequest request) {
+		logger.info("Received country selection: " + nationality);
+
+		HttpSession session = request.getSession(); // 세션 객체 가져오기
+		session.setAttribute("selectedCountry", nationality); // 세션에 "selectedCountry"라는 이름으로 국가 정보 저장
+		// 헤더의 국가 선택 셀렉트 박스에서
+		// - 일본 선택 시: selectedCountry 세션 속성에 "jp" 저장
+		// - 한국 선택 시: selectedCountry 세션 속성에 "kr" 저장
+
+		logger.info("Saved nationality '" + nationality + "' to session.");
+
+		return "success"; // 클라이언트(JavaScript)로 성공 응답 보내기
+	}
+	
+}
